d867786296cc7f4c3594a9dccbc3d5b9e6204ea2
Philip Soriano <philip.soriano@hotmail.com>
2021-08-21 20:38:44 +0400

    testing models

--- backend/api/models.py
+++ backend/api/models.py
@@ -21,3 +21,4 @@ class Task(models.Model):
     def __str__(self):
         return f"{self.title} (Task) - {self.user}"
 
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Task, Project
+from django.contrib.auth.models import User
 # Create your views here.
+
+def listData(request):
+    queryset_users = User.objects.all()
+    queryset_tasks = Task.objects.all()
+    queryset_project = Project.objects.all()
+    context = {
+        "users": queryset_users,
+        "tasks": queryset_tasks,
+        "projects": queryset_project
+    }
+    return render(request, 'api/test.html', context)
