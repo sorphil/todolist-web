8cc91c0968d6fa8b83eab7e2b8f6513da0f7e8ea
= <=>
2022-01-29 21:57:06 +0400

    fixed api url

--- frontend/src/utils/apiCaller.js
+++ frontend/src/utils/apiCaller.js
@@ -3,7 +3,7 @@ import tokenHandler from "./tokenHandler"
 const apiCaller = (()=>
 {
     let headers = tokenHandler.headers
-    let apiURL = 'http://sorphil.pythonanywhere.com/todos/api/v1/'
+    let apiURL = 'https://sorphil.pythonanywhere.com/todos/api/v1/'
 
     const postCall = (prefix, type, body)=>
     {
