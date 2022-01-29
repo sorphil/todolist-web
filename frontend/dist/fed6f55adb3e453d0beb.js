9ef644459393bb3afc647f45bf791012f33ae40e
= <=>
2022-01-29 21:29:07 +0400

    Add files

---
+++ frontend/dist/fed6f55adb3e453d0beb.js
@@ -0,0 +1,19 @@
+import functionInterface from "./functionInterface";
+import "./styles/styles.scss"; // document.addEventListener('DOMContentLoaded', ()=>{
+//     functionInterface.checkUserStatus()
+//     //authenticationForm('formname', hasErrors)
+//     functionInterface.authenticationForm('register', true)
+//     
+// 
+//     // functionInterface.authenticationForm('check', false)
+//     functionInterface.taskForm('task-create')
+//     functionInterface.projectForm('project-create')
+// })
+
+document.addEventListener('DOMContentLoaded', function () {
+  //
+  // functionInterface.authenticationForm('logout', false)
+  // functionInterface.checkUserStatus()
+  // console.log(functionInterface.checkUserStatus())
+  functionInterface.startingPage();
+});
